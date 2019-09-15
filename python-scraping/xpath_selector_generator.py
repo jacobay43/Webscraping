77d05a7f6547ac85e73be751543f16485cf2883c
Opegbemi Ayodele Jacob <jacobay43@gmail.com>
2019-09-15 10:04:18 +0100

    Update xpath_selector_generator from v2 to v3
    More efficient exception handling
    new keywords: ignore_case, _filter
    More robust, can handle more cases

--- python-scraping/xpath_selector_generator.py
+++ python-scraping/xpath_selector_generator.py
@@ -1,19 +1,18 @@
 import re
-from lxml.html import fromstring
+import lxml
 from textblob import TextBlob
-import time
-'''
-Drawback: Extremely slow for complex site like Eventbrite
-		  Considerably slow for multiple word keyword arguments
-'''
-def return_selectors(html, keyword):
+from bot_utilities import get_info_from_page
+
+def return_selectors(html, keyword, _filter=['img'], ignore_case=True):
     '''
     Receive an html file's contents and string and return the corresponding xpath selector
     args:
         html (str): html content downloaded from a network or opened from a file stream
         keyword (str): string in focus from the webpage whose xpath selector is needed
+        _filter (list): list of tags to filter out of result
+        ignore_case (bool): determines whether exact case should be matched when looking for expression
     returns:
-        if found: list of selectors like '//div[@id="wanem"]' will be returned
+        if found: list of selectors like ['//div[@id="wanem"]'] will be returned
         if not found: '//error', '//Missing'
     NB: regex only works on str not bytes in case file is read in 'rb' mode
         case 3 does not pose a problem as long as another attribute aside class|id is defined properly
@@ -23,51 +22,81 @@ def return_selectors(html, keyword):
         html = str(html)
         
         #regex below selects collection of whole strings up to keyword
-        keyline = re.compile(r'(.*?)%s'%keyword, re.IGNORECASE).findall(html)#[0]
+        if ignore_case:
+            keyline = re.compile(r'(.*?)\s*%s'%keyword, re.IGNORECASE).findall(html)#[0]
+        else:
+            keyline = re.compile(r'(.*?)\s*%s'%keyword).findall(html)
         #regex below selects parent tag of keyword
+        keyline += [html[(html.index(keyword)-100):html.index(keyword)]] #also consider tag before tag appearing to have content because of e.g <a href='link'><i id="g"></i>Content   :if you do not do this, only i is considered which is actually empty
         parent_tags = []
         if len(keyline) > 0:
             for line in keyline:
-                parent_tags += re.compile(r'<[^/].*?\".*?\"', re.IGNORECASE).findall(line)#[-1] #[-1] avoids case 1 by forcing it to use the last match from the top of the document, which will be keyword's parent tag
+                #print('On line:',line)
+                if ignore_case:
+                    parent_tags += re.compile(r'<[^/].*?\".*?\"', re.IGNORECASE).findall(line)
+                    #parent_tags += re.compile(r'<[^/].*?\".*?\"', re.IGNORECASE).findall(line)#[-1] #[-1] avoids case 1 by forcing it to use the last match from the top of the document, which will be keyword's parent tag
+                else:
+                    parent_tags += re.compile(r'<[^/].*?\".*?\"').findall(line)
+                    #parent_tags += re.compile(r'<[^/].*?\".*?\"').findall(line)
         selectors = []
-    
+        #print('parent_tags:',' '.join(parent_tags),sep='\n')
         for parent_tag in parent_tags:
-            if len(parent_tag) > 0:
-                #.split('>')[-1] removes risk of case 2 using h5 in selector
-                parent_tag = parent_tag.split('>')[-1] if len(parent_tag.split('>')[-1]) > 0 else parent_tag.split('>')[-2]
-                #the next 3 lines solve problem of attribute value containing spaces by extracting the attribute value(the pattern) seperately from the tag and ref (WordList)
-                pattern = (parent_tag.split('"')[-1] if len(parent_tag.split('"')[-1]) > 0 else parent_tag.split('"')[-2]) #extract pattern from string
-                parent_tag = parent_tag[:parent_tag.index('=')] #only consider part of string with tag and ref
-                tag, ref = TextBlob(parent_tag).words
-                selectors += [f'//{tag}[@{ref}="{pattern}"]']
-                #print(selectors[-1])
-        return selectors
+            try:
+                discard = False
+                if len(parent_tag) > 0:
+                    #.split('>')[-1] removes risk of case 2 using h5 in selector
+                    ind = -1
+                    if len(parent_tag.split('>')[ind]) > 0:
+                        parent_tag = parent_tag.split('>')[ind]
+                    else:
+                        ind -= 1
+                        while not len(parent_tag.split('>')[ind]) > 0:
+                            ind -= 1
+                        parent_tag = parent_tag.split('>')[ind]
+                    #parent_tag = parent_tag.split('>')[-1] if len(parent_tag.split('>')[-1]) > 0 else parent_tag.split('>')[-2]
+                    #the next 3 lines solve problem of attribute value containing spaces by extracting the attribute value(the pattern) seperately from the tag and ref (WordList)
+                    pattern = (parent_tag.split('"')[-1] if len(parent_tag.split('"')[-1]) > 0 else parent_tag.split('"')[-2]) #extract pattern from string
+                    parent_tag = parent_tag[:parent_tag.index('=')] #only consider part of string with tag and ref
+                    if len(TextBlob(parent_tag).words) == 2:
+                        tag, ref = TextBlob(parent_tag).words
+                        for t in _filter:
+                            if tag == t:
+                                print('Discarding',tag)
+                                discard = True
+                                break
+                        if discard == True:
+                            continue
+                        selectors += [f'//{tag}[@{ref}="{pattern}"]']
+                    else:
+                        print('Discarding',parent_tag[:10],'for being too long')
+                    #print(selectors[-1])
+            except Exception as e:
+                print(e,'for',parent_tag[:10])
+        return list(set(selectors))
     except Exception as e:
         print(e)
-        print(parent_tag)
         return '//error'
     return "//Missing"
     
 if __name__ == '__main__':
-	with open('webpage.txt','r') as file:
-		print('\n'.join(return_selectors(file.read(), 'Upcoming Events')))
-    startTime = time.time()        
-    case_1 = '''<h5>
-                                              <div class="layer-unwrap">Hello
-                                              </div>
-                             </h5>'''
-    case_2 = '''<h5><div class="layer-rap">Hello</div>
-                             </h5>'''
-    case_3 = '''<p text-font = "12px" class = ".nodec">Greet your friend for me</p>'''
-
-    print('Selector:',return_selectors(case_1, 'Hello'))
-    print(f'Time Elapsed: {time.time()-startTime:.2f} s')
-    startTime = time.time()
-    print('Selector:',return_selectors(case_2, 'Hello'))
-    print(f'Time Elapsed: {time.time()-startTime:.2f} s')
-    startTime = time.time()
-    print('Selector:',return_selectors(case_3, 'Greet'))
-    print(f'Time Elapsed: {time.time()-startTime:.2f} s')
-    print(fromstring(case_3).xpath(return_selectors(case_3,'Greet')[0])[0].text_content())
-
-
+    '''
+    To filter out multispaces:
+        ' '.join(str.split())
+        ' '.join(' '.join(list).split())
+    '''
+    print('Opening file')
+    with open('links.txt','r') as file:
+        keyword = 'PENCIL'
+        print('Generating selectors for',keyword)
+        selectors = return_selectors(file.read(), keyword, ignore_case=False)
+        print(len(selectors),'selectors generated for',keyword)
+        for selector in selectors:
+            try:
+                file.seek(0)
+                content = get_info_from_page(file.read(), selector=selector)
+                if any((x.isalpha() for x in ' '.join(content))):
+                    print(selector,'content:',' '.join(' '.join(content).split()))
+                else:
+                    print(selector,'is empty')
+            except lxml.etree.ParserError as e:
+                print('For',selector,e)
